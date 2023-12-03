a481a3faac36f13902baf07208752a84dd744809
Elica Putri <elicaputri0703@gmail.com>
2023-12-03 19:51:25 +0800

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Android\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\flutter apps\\aplikasi monitoring\\monitor_broiler" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Android\\flutter"
+  "PROJECT_DIR=D:\\flutter apps\\aplikasi monitoring\\monitor_broiler"
+  "FLUTTER_ROOT=C:\\Android\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\flutter apps\\aplikasi monitoring\\monitor_broiler\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\flutter apps\\aplikasi monitoring\\monitor_broiler"
+  "FLUTTER_TARGET=D:\\flutter apps\\aplikasi monitoring\\monitor_broiler\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC85MGZhM2FlMjhmZTZkZGFlZTFhZjJjMTIwZjAxZTUwMjAxYzE0MDFiLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\flutter apps\\aplikasi monitoring\\monitor_broiler\\.dart_tool\\package_config.json"
+)
